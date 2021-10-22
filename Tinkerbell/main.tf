5cd4bbca942d1a2fc43914a183edfce7c5af2f42
Kristian Jones <me@kristianjones.dev>
2021-10-22 21:50:43 +0000

    fix

--- Tinkerbell/main.tf
+++ Tinkerbell/main.tf
@@ -16,3 +16,87 @@ provider "tinkerbell" {
   grpc_authority = "tink-grpc-cont.service.kjdev:42113"
   cert_url       = "http://tink-http-cont.service.kjdev:42114/cert"
 }
+
+resource "tinkerbell_hardware" "foo" {
+  data = <<EOF
+{
+  "id": "2bd4b2b3-3104-4f67-8b5c-3d208d9cd1cd",
+  "metadata": {
+    "facility": {
+      "facility_code": "onprem",
+      "plan_slug": "c2.medium.x86",
+      "plan_version_slug": ""
+    },
+    "instance": {},
+    "state": "provisioning"
+  },
+  "network": {
+    "interfaces": [
+      {
+        "dhcp": {
+          "arch": "x86_64",
+          "ip": {
+            "address": "172.31.241.33",
+            "gateway": "172.31.241.37",
+            "netmask": "255.255.255.224"
+          },
+          "mac": "b8:ae:ed:79:5e:1d"
+        },
+        "netboot": {
+          "allow_pxe": true,
+          "allow_workflow": true
+        }
+      }
+    ]
+  }
+}
+EOF
+}
+
+resource "tinkerbell_template" "foo" {
+  name    = "foo"
+  content = <<EOF
+version: "0.1"
+name: ubuntu_provisioning
+global_timeout: 6000
+tasks:
+  - name: "os-installation"
+    worker: "{{.device_1}}"
+    volumes:
+      - /dev:/dev
+      - /dev/console:/dev/console
+      - /lib/firmware:/lib/firmware:ro
+    environment:
+      MIRROR_HOST: http-cont.service.kjdev:8080
+    actions:
+      - name: "disk-wipe"
+        image: disk-wipe
+        timeout: 90
+      - name: "disk-partition"
+        image: disk-partition
+        timeout: 600
+        environment:
+          MIRROR_HOST: http-cont.service.kjdev:8080
+        volumes:
+          - /statedir:/statedir
+      - name: "install-root-fs"
+        image: install-root-fs
+        timeout: 600
+      - name: "install-grub"
+        image: install-grub
+        timeout: 600
+        volumes:
+          - /statedir:/statedir
+EOF
+}
+
+resource "tinkerbell_workflow" "foo" {
+  template  = tinkerbell_template.foo.id
+  hardwares = <<EOF
+{"device_1":"b8:ae:ed:79:5e:1d"}
+EOF
+
+  depends_on = [
+    tinkerbell_hardware.foo,
+  ]
+}
