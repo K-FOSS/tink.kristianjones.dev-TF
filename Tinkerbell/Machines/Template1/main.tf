2ac7eb6be84eecb7328c95346f48d04628f4d1fb
Kristian Jones <me@kristianjones.dev>
2021-10-30 20:19:02 +0000

    fix

--- Tinkerbell/Machines/Template1/main.tf
+++ Tinkerbell/Machines/Template1/main.tf
@@ -59,7 +59,7 @@ resource "tinkerbell_hardware" "Hardware" {
           "name_servers": [
             "172.16.100.25"
           ],
-          "uefi": false
+          "uefi": true
         },
         "netboot": {
           "allow_pxe": true,
